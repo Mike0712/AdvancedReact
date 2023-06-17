7e1bd10cf7a3f05362120225d9156092e1d6bc95
mike <barrister2003@gmail.com>
2023-06-18 01:00:27 +0300

    add tests for login module

---
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { Dispatch } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { loginByUsername } from './loginByUsername';
+
+jest.mock('axios');
+
+const mocketdAxios = jest.mocked(axios, true);
+
+describe('loginByUsername.test', () => {
+    // let dispatch: Dispatch;
+    // let getState: () => StateSchema;
+
+    // beforeEach(() => {
+    //     dispatch = jest.fn();
+    //     getState = jest.fn();
+    // });
+
+    // test('success login', async () => {
+    //     const userValue = {
+    //         username: 'admin', id: '1',
+    //     };
+    //     mocketdAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+    //     const action = loginByUsername({ username: 'admin', password: '123' });
+    //     const result = await action(dispatch, getState, undefined);
+
+    //     expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+    //     expect(dispatch).toHaveBeenCalledTimes(3);
+    //     expect(mocketdAxios.post).toHaveBeenCalled();
+    //     expect(result.meta.requestStatus).toBe('fulfilled');
+    //     expect(result.payload).toEqual(userValue);
+    // });
+
+    // test('fail login', async () => {
+    //     mocketdAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+    //     const action = loginByUsername({ username: 'admin', password: '123' });
+    //     const result = await action(dispatch, getState, undefined);
+
+    //     expect(dispatch).toHaveBeenCalledTimes(2);
+    //     expect(mocketdAxios.post).toHaveBeenCalled();
+    //     expect(result.meta.requestStatus).toBe('rejected');
+    //     expect(result.payload).toBe('error');
+    // });
+
+    test('success login', async () => {
+        const userValue = {
+            username: 'admin', id: '1',
+        };
+        mocketdAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: 'admin', password: '123' });
+
+        expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+        expect(thunk.dispatch).toHaveBeenCalledTimes(3);
+        expect(mocketdAxios.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(userValue);
+    });
+
+    test('fail login', async () => {
+        mocketdAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: 'admin', password: '123' });
+
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(mocketdAxios.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toBe('error');
+    });
+});
---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,22 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'John' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('David'),
+        )).toEqual({ username: 'David' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('321'),
+        )).toEqual({ password: '321' });
+    });
+});
